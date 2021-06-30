4355c13fd77f5873ae284c05104a496182835eb0
doug <dougparker.m@gmail.com>
2021-06-30 10:14:22 -0230

    Organized main to better implement functions and avoid errors. isBusted error seems resolved.

--- blackjack.py
+++ blackjack.py
@@ -3,10 +3,7 @@ import db as db
 
 
 def main():
-    deck = bf.createDeck()
-    dealerHand = bf.dealHandOfCards(deck)
-    playerHand = bf.dealHandOfCards(deck)
-    willHit = True
+
     playAgain = True
 
     bf.displayTitleInfo()
@@ -15,11 +12,18 @@ def main():
         if(bf.isBankrupt()):
             bf.topUpBank()
 
+        #the deck needs to be created inside the loop
+        #otherwise, the pop() will slowly remove all cards from the deck
+        deck = bf.createDeck()
+        dealerHand = bf.dealHandOfCards(deck)
+        playerHand = bf.dealHandOfCards(deck)
+
         wager = bf.getPlayerWager()
         bf.displayDealerShowCard(dealerHand)
 
         #handle all player actions here
         #modifying the player hand before coming to the end
+        willHit = True
         while willHit:
             bf.displayPlayerHand(playerHand)
             willHit = bf.decideHitOrStand(deck, playerHand)
